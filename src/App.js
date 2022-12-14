d29d44e69ee8340db08cd7e2200dcd667ff73d79
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2022-12-14 16:45:39 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {wordsList} from './data/word'
 import StartScreen from './components/StartScreen';
 import Game from './components/Game';
 import GameOver from './components/GameOver';
+import AnimCursor from './components/AnimCursor';
 
 
 const stages = [
@@ -18,6 +19,8 @@ const stages = [
   {id: 2 , name:'game'},
   {id: 3 , name:'end'}
 ]
+
+const guessesQty = 3;
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
@@ -27,8 +30,14 @@ function App() {
   const [pickedCategory, setPickedCategory] = useState('');
   const [letters, setLetters] = useState('');
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(guessesQty);
+  const [score, setScore] = useState(0);
+  
 
-  const pickWordAndPickCategory = () => {
+
+  const pickWordAndPickCategory = useCallback(() => {
     // pick a random category
     const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
@@ -39,10 +48,13 @@ function App() {
     console.log(word)
 
     return {word, category};
-  }
+  }, [words]);
 
   // starts the secret word game
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    // clear all letters
+    clearLetterStates();
+
     // pick word and pick category
     const {word, category} = pickWordAndPickCategory();
 
@@ -51,33 +63,100 @@ function App() {
 
     wordLetters = wordLetters.map((l) => l.toLowerCase());
 
-    console.log(wordLetters)
+    console.log(wordLetters);
     console.log(word, category);
 
     // fill states
     setPickedWord(word);
     setPickedCategory(category);
-    setLetters(letters);
+    setLetters(wordLetters);
     
     setGameStage(stages[1].name);
-  }
+  }, [pickWordAndPickCategory]);
 
 
   // process the letter input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
-  }
+  const verifyLetter = (letter) => {
+    
+    const normalizedLetter = letter.toLowerCase();
+
+    // check if letter has alredy been utilized
+    if (guessedLetters.includes(normalizedLetter) ||
+    wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+
+    //push guessed letter or remove a guess
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter
+      ])
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
+  };
+  
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+
+  // check if guesses ended
+  useEffect(() => {
+    if (guesses <= 0) {
+      // reset all states
+      clearLetterStates()
+
+      setGameStage(stages[2].name);
+    }
+  }, [guesses])
+
+  // check win condition
+  useEffect(() => {
+
+    const uniqueLetters = [...new Set(letters)];
+
+    //win condition
+    if (guessedLetters.length === uniqueLetters.length) {
+      // add score
+      setScore((actualScore) => (actualScore += 100));
+
+      // restart game with new word
+      startGame();
+    }
+  }, [guessedLetters, letters, startGame])
 
   // restarts the game
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+
     setGameStage(stages[0].name);
   }
 
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
-      {gameStage === 'end' && <GameOver retry={retry}/>}
+      {gameStage === 'game' && (<Game 
+      verifyLetter={verifyLetter}
+      pickedWord={pickedWord}
+      pickedCategory={pickedCategory}
+      letters={letters}
+      guessedLetters={guessedLetters}
+      wrongLetters={wrongLetters}
+      guesses={guesses}
+      score={score}
+        />
+      )}
+      {gameStage === 'end' && <GameOver retry={retry} score={score}/>}
+      <AnimCursor/>
     </div>
   );
 }
---
+++ src/components/AnimCursor.js
@@ -0,0 +1,32 @@
+import AnimatedCursor from "react-animated-cursor";
+
+
+const AnimCursor = () => {
+  return (
+    <AnimatedCursor
+      innerSize={10}
+      outerSize={30}
+
+      color='0, 247, 255'
+      
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]}
+    />
+  )
+}
+
+export default AnimCursor;
